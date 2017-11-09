b29816f17c4ea1cc1d3bfe2602f07aae82d0c712
Bonstark <31876796+Bonstark@users.noreply.github.com>
2017-11-09 22:05:10 +0900

    Add files via upload
    Added hidden section (class="hiddenOnLoad"), changed image animation.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
         $("#container").fadeIn();
+        var box=$("#right");
+        var boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2];
 
+  $(document).mousemove(function(e){
+	   var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);
+     var angle = angle-180
+     box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});
+     box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
+     box.css({ 'transform': 'rotate(' + angle + 'deg)'});
+   });
 
-$("#right").mouseenter(function(){
-	$(this).addClass("rotate").delay(3000).queue(function(){
-		$(this).removeClass("rotate").dequeue();
-	});
-  });
+   $(".switch").click(function() {
+     $("#leftTop, #leftBottom").fadeOut("normal", function() {
+       $(this).css("display", "none");
+       $(".hiddenOnLoad").show();
+     });
+   });
+
+   $(".return").click(function() {
+     $("#leftTopHidden, #leftBottomHidden").fadeOut("normal", function() {
+      $(this).css("display", "none");
+      $("#leftTop, #leftBottom").css("display", "inline-block");
+      });
+    });
+
 });
